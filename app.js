510a28ba25e2490c5a1d35a90e3e9298a67eb7aa
Erin <erinedawson@gmail.com>
2017-08-08 11:00:23 -0400

    add functionality to change top meme caption

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 /*
-build out your main functionality inside function 'main' first, 
+build out your main functionality inside function 'main' first,
 then you can just run document ready on the function 'main' at the bottom of the page
 */
 
 
 var main = function(){
+  $("#top-bit").keyup(function(){
+    $topBit = $("#top-bit").val();
+    $(".top-line").text($topBit);
+  });
 
 };
 
