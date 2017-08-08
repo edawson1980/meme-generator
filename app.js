a39d82ac1f06416acda42d20953b1b3012678d6d
Erin <erinedawson@gmail.com>
2017-08-07 20:05:42 -0400

    first commit.  set up index.html, style.css, app.js.  import jquery

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+build out your main functionality inside function 'main' first, 
+then you can just run document ready on the function 'main' at the bottom of the page
+*/
+
+
+var main = function(){
+
+};
+
+$(document).ready(main);
